9384952b1315baddaf0df93502f9c600e6b212cc
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2017-05-29 12:44:37 +0300

    fix backend error router

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../dist'));
 
-// app.use(router);
+app.use(router);
 
 http.listen(PORT, function () {
   console.log('Example app listening on port', PORT);
