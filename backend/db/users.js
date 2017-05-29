1c07d2074120a2c0c3d637995299d7f8117bb185
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2017-05-30 02:14:22 +0300

    prevent sql injections

--- backend/db/users.js
+++ backend/db/users.js
@@ -1,25 +1,27 @@
 let connection = require("./connection.js");
+let mysql = require('mysql');
+
 
 let users = function () {
   self = this;
 
   self.getUserById = function (id) {
-    return connection.query("SELECT * FROM users WHERE ID = " + id + ";");
+    return connection.query("SELECT * FROM users WHERE ID = " + mysql.escape(id) + ";");
   };
 
   self.getUserByEmail = function (email) {
-    return connection.query("SELECT * FROM users WHERE email = \'" + email + "\';");
+    return connection.query("SELECT * FROM users WHERE email = " + mysql.escape(email) + ";");
   };
 
   self.addUser = function (user) {
     return connection.query("INSERT INTO users( name, email, password)" +
-      "VALUES(\'" + user.name + "\', \'" + user.email + "\', \'" + user.password + "\');");
+      "VALUES(" + mysql.escape(user.name) + ", " + mysql.escape(user.email) + ", " + mysql.escape(user.password) + ");");
   };
 
   self.getLastId = function () {
     return connection.query("SELECT \"id\" FROM \"users\" ORDER BY \"id\" DESC LIMIT 1;");
   };
-  
+
   self.getAllUsers = function () {
     return connection.query("SELECT * FROM users;");
   };
