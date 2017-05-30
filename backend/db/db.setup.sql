b0d7e4a5a1a621f3867ce60366b0f9ae67da58a7
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2017-05-30 10:35:03 +0300

    insert two users in sql script

--- backend/db/db.setup.sql
+++ backend/db/db.setup.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS itinarray_test;
 
 USE itinarray_test;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     ID int NOT NULL AUTO_INCREMENT,
     name varchar(30) NOT NULL,
     email varchar(30) UNIQUE NOT NULL,
@@ -10,3 +10,6 @@ CREATE TABLE users (
     role ENUM('user', 'admin') DEFAULT 'user' NOT NULL,
     PRIMARY KEY (ID)
 );
+
+INSERT INTO users (name,email, password, role) VALUES('test', 'test@test.com', '$2a$10$0YC.fQvCmWNMruKLSoaZGuQgjnBLilYreYN8W5McjT0gEjAD9EBzi', 'user');
+INSERT INTO users (name,email, password, role) VALUES('admin', 'admin@admin.com', '$2a$10$0YC.fQvCmWNMruKLSoaZGuQgjnBLilYreYN8W5McjT0gEjAD9EBzi', 'admin');
